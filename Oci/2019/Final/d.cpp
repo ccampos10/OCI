de0945d7636b9c071a61ad280c1ecccadbef7b56
ccampos10 <ccampos.pisis10@gmail.com>
2022-10-27 18:45:16 -0300

    codigo 2019/d

--- Oci/2019/Final/d.cpp
+++ Oci/2019/Final/d.cpp
@@ -1,1 +1,19 @@
 //
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int m, n, k, j, y, x;
+    int turno = 1;
+    bool vive = true;
+    cin >> m >> n >> k;
+    vector<vector<int>> plano(m,vector<int>(n,0));
+    vector<vector<int>> rocas;
+    for(int i = 0; i < k; i++){
+        cin >> j >> y >> x;
+        rocas.push_back({j,y,x});
+    }
+
+    return 0;
+}
